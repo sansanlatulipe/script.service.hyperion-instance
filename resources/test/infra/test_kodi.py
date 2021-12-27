9a72f00ad9964127420b1369d0ad5e7248e54b10
SansanLaTulipe <dpelliss@gmail.com>
2021-12-27 19:26:50 +0100

    Fix led.manager name in tests

--- resources/test/infra/test_kodi.py
+++ resources/test/infra/test_kodi.py
@@ -12,34 +12,34 @@ class MyMonitorShould(unittest.TestCase):
 
 
 class MyPlayerShould(unittest.TestCase):
-    @mock.patch('resources.lib.service.led.Controller')
-    def setUp(self, ledController):
-        self.ledController = ledController
-        self.player = kodi.MyPlayer(ledController)
+    @mock.patch('resources.lib.service.led.Manager')
+    def setUp(self, ledManager):
+        self.ledManager = ledManager
+        self.player = kodi.MyPlayer(ledManager)
 
     def test_turn_led_on_when_start_playing_video(self):
-        self.ledController.on = mock.Mock(return_value=None)
+        self.ledManager.on = mock.Mock(return_value=None)
         self.player.setPlayingMode('video')
 
         self.player.onPlayBackStarted()
         self.player.onPlayBackResumed()
 
-        self.assertEqual(2, self.ledController.on.call_count)
+        self.assertEqual(2, self.ledManager.on.call_count)
 
     def test_not_turn_led_on_when_start_playing_music(self):
-        self.ledController.on = mock.Mock(return_value=None)
+        self.ledManager.on = mock.Mock(return_value=None)
         self.player.setPlayingMode('music')
 
         self.player.onPlayBackStarted()
         self.player.onPlayBackResumed()
 
-        self.ledController.on.assert_not_called()
+        self.ledManager.on.assert_not_called()
 
     def test_turn_led_off_when_stop_playing(self):
-        self.ledController.off = mock.Mock(return_value=None)
+        self.ledManager.off = mock.Mock(return_value=None)
 
         self.player.onPlayBackStopped()
         self.player.onPlayBackEnded()
         self.player.onPlayBackPaused()
 
-        self.assertEqual(3, self.ledController.off.call_count)
+        self.assertEqual(3, self.ledManager.off.call_count)
