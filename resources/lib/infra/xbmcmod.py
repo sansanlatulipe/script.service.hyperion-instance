f3831e313e92cbba36c9b4a33edbf825ad53a4c2
SansanLaTulipe <dpelliss@gmail.com>
2021-12-28 19:24:07 +0100

    Clean xbmcmod

--- resources/lib/infra/xbmcmod.py
+++ resources/lib/infra/xbmcmod.py
@@ -3,27 +3,17 @@
 
 try:
     from xbmc import log, LOGDEBUG, LOGINFO, LOGWARNING, LOGERROR
-    from xbmc import executeJSONRPC
     from xbmc import Monitor, Player
     from xbmcaddon import Addon
-    from xbmcgui import Dialog, NOTIFICATION_INFO, NOTIFICATION_WARNING, NOTIFICATION_ERROR
-    from xbmcgui import DialogProgressBG
-    from simplecache import SimpleCache
+    from xbmcgui import Dialog
 except ImportError:
     LOGDEBUG = 'DEBUG'
     LOGINFO = 'INFO'
     LOGWARNING = 'WARNING'
     LOGERROR = 'ERROR'
-    NOTIFICATION_INFO = 'INFO'
-    NOTIFICATION_WARNING = 'WARNING'
-    NOTIFICATION_ERROR = 'ERROR'
 
     def log(msg, lvl):
         print(lvl, msg)
-
-
-    def executeJSONRPC(query):
-        return '{"result":{"movies":[],"moviedetails":{}}}'
 
 
     class Monitor:
@@ -54,7 +44,7 @@ except ImportError:
                 'hyperion_ip': '127.0.0.1',
                 'hyperion_port': '8090',
                 'hyperion_token': None,
-                'hyperion_instance': 1
+                'hyperion_instance': '1'
             }
 
         def getAddonInfo(self, key):
@@ -74,28 +64,3 @@ except ImportError:
     class Dialog:
         def ok(self, heading, text):
             print('{} > {}'.format(heading, text))
-
-        def notification(self, heading, text, icon=NOTIFICATION_INFO, time=5000, sound=True):
-            print('[{}] {} > {}'.format(icon, heading, text))
-
-
-    class DialogProgressBG:
-        def create(self, heading, msg):
-            print('{} > {}'.format(heading, msg))
-
-        def update(self, percent, heading=None, msg=None):
-            print('{} > {} ({}%)'.format(heading, msg, percent))
-
-        def close(self):
-            pass
-
-
-    class SimpleCache:
-        def __init__(self):
-            self.cached = {}
-
-        def get(self, key):
-            return self.cached.get(key)
-
-        def set(self, key, value):
-            self.cached[key] = value
